e6c7fb483880432a6ae10db75e99af25f71dee0d
Brendan O'Dea <brendan.ode4@gmail.com>
2018-02-07 00:18:37 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,5 +10,30 @@ class Song
     song.artist_name = file.split("-")[0].strip
     song
   end
-end
 
+  
+  require 'pry'
+  class Song
+    attr_accessor :name, :artist
+    # attr_reader :
+  
+    def initialize(name)
+      @name = name
+    end
+  
+    def self.new_by_filename(filename)
+      song_title = filename.split(" - ")[1]
+      song_artist = filename.split(" - ")[0]
+      song = Song.new(song_title)
+      song.artist_name = song_artist
+      
+      song
+    end
+  
+    def artist_name=(name)
+      artist = Artist.find_or_create_by_name(name)
+      self.artist = artist
+      artist.add_song(self)
+    end
+  
+end
