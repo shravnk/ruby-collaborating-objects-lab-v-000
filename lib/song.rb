d7784e98ca031b69eb1fa949c841f8c3c27d307b
Brendan O'Dea <brendan.ode4@gmail.com>
2018-02-07 00:20:25 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,29 +11,17 @@ class Song
     song
   end
 
-  
-  require 'pry'
-  class Song
-    attr_accessor :name, :artist
-    # attr_reader :
-  
-    def initialize(name)
-      @name = name
-    end
-  
-    def self.new_by_filename(filename)
-      song_title = filename.split(" - ")[1]
-      song_artist = filename.split(" - ")[0]
-      song = Song.new(song_title)
-      song.artist_name = song_artist
-      
-      song
-    end
-  
-    def artist_name=(name)
-      artist = Artist.find_or_create_by_name(name)
-      self.artist = artist
-      artist.add_song(self)
-    end
-  
+  def artist_name=(name)
+    artist = Artist.find_or_create_by_name(name)
+    self.artist = artist
+    artist.add_song(self)
+  end
+
 end
+
+  
+
+  
+
+  
+
