3447b9c46c63dde92373ebbae22e5194692dad06
Brendan O'Dea <brendan.ode4@gmail.com>
2018-02-07 00:17:07 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,4 +4,10 @@ class Song
   def initialize(title)
     @title = title
   end
+
+  def self.new_by_filename(filename)
+    song = self.new(file.split("-")[1].strip) 
+    song.artist_name = file.split("-")[0].strip
+    song
+  
 end
