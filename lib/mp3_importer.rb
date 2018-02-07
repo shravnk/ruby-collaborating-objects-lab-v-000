a32441a64c7473eb570b79b3e0356fb269bfcec3
Brendan O'Dea <brendan.ode4@gmail.com>
2018-02-07 00:38:04 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -16,9 +16,4 @@ class MP3Importer
       Song.new_by_filename(file)
     end
   end
-
 end
-
-test_music_path = "./spec/fixtures/mp3s"
-music_importer = MP3Importer.new(test_music_path)
-music_importer.import
