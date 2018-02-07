e657004abaec934c7ec99e34fd002967d580f50e
Brendan O'Dea <brendan.ode4@gmail.com>
2018-02-07 00:33:02 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -18,7 +18,7 @@ class MP3Importer
 
   def import
     self.files.each do |file|
-      
+
       Song.new_by_filename(file)
       binding.pry
     end
--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,3 @@
-require 'pry'
-require_relative "../lib/artist.rb"
-require_relative "../lib/song.rb"
-require_relative "../lib/mp3_importer.rb"
-
 class Song
   attr_accessor :artist, :name
 
